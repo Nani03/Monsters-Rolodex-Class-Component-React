fb0c5d31ccaabf3a43674e72c55d4ef89d5e310d
nani03 <leela.rroyal@gmail.com>
2023-03-20 15:33:44 +0530

    key render change

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ const CardList = (props) => {
   return (
     <div className="card-list">
       {monsters.map((monster) => {
-        return <Card monster={monster} />;
+        return <Card key={monster.id} monster={monster} />;
       })}
     </div>
   );
